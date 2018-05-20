952951ada6a4b83152e27131859650dac1714613
jodth07 <jodth09@gmail.com>
2018-05-19 21:31:12 -0400

    updating folder

--- Application/lexicon/views.py
+++ Application/lexicon/views.py
@@ -3,4 +3,6 @@ from flask import Blueprint
 lexicon_blueprint = Blueprint('lexicon', __name__, template_folder='templates')
 
 
-@lexicon_blueprint.route('')
+@lexicon_blueprint.route('dictionary')
+def lexicon():
+    return "This is the lexicon Page"
